e5d3e5f26ddf7df19fe059a8d582e77466e75762
Gustavo Lima <gustavomdcl@gmail.com>
2019-06-30 15:21:32 -0300

    User authentication initialized

--- modulo02_2/src/routes.js
+++ modulo02_2/src/routes.js
@@ -3,6 +3,7 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
@@ -17,6 +18,8 @@ const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 // module.exports = routes;
 // OU (com sucrase)
